73c42c2698dc3e2e2522a477c641dc71c32dfc4f
bisma03 <bismaabdulgani030301@gmail.com>
2024-12-22 20:32:19 +0700

    Add files via upload

---
+++ Tugas 4/Mandiri 2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	printf("hasil N");
+	int N;
+	scanf("N");
+	if(N>50) {
+		printf("N=N+10");
+		
+	}else {
+		printf("N=N-25");
+	}
+printf("\n\nprogram selesai.");
+return 0;
+}
